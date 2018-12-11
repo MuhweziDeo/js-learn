97c1a7e1d402fc77adef8dc253337736c120c18f
muhwezi deo <aggrey256@gmail.com>
2018-12-11 15:57:24 +0300

    setup project

---
+++ react-demo/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+class App extends React.Component{
+  render(){
+return (
+<h1>Hello</h1>
+);
+}
+
+}
+export default App;
