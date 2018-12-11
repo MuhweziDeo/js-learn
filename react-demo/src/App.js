bb632acf92f6b085f80147d73b46e96ccaadfc96
muhwezi deo <aggrey256@gmail.com>
2018-12-11 16:26:06 +0300

    create component table and render it

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Table';
+import Table from './Table';
 class App extends React.Component{
   render(){
     var name='dee';
 return (
 
+
+<div className="container">
 <h1>Hello {name}</h1>
+<Table/>
+</div>
+
 );
 }
 
