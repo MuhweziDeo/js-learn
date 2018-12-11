68e96475ee57fd6cb2a6a051002150739282ab92
muhwezi deo <aggrey256@gmail.com>
2018-12-11 19:56:00 +0300

    add state

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -3,32 +3,33 @@ import './App.css';
 import './Table';
 import Table from './Table';
 class App extends Component{
+  state={
+    characters:[
+      {
+        'name': 'Charlie',
+        'job': 'Janitor'
+    },
+    {
+        'name': 'Mac',
+        'job': 'Bouncer'
+    },
+    {
+        'name': 'Dee',
+        'job': 'Aspring actress'
+    },
+    {
+        'name': 'Dennis',
+        'job': 'Bartender'
+    }
+    ]
+  };
   render(){
     var name='dee';
-    const characters = [
-      {
-          'name': 'Charlie',
-          'job': 'Janitor'
-      },
-      {
-          'name': 'Mac',
-          'job': 'Bouncer'
-      },
-      {
-          'name': 'Dee',
-          'job': 'Aspring actress'
-      },
-      {
-          'name': 'Dennis',
-          'job': 'Bartender'
-      }
-  ];
+   
 return (
-
-
 <div className="container">
 <h1>Hello {name}</h1>
-<Table characterData={characters}/>
+<Table characterData={this.state.characters}/>
 </div>
 
 );
