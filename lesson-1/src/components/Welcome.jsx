05b817b97522cd9b9dfb8ffb1e494720fc8d5abf
muhwezi deo <aggrey256@gmail.com>
2018-12-15 15:03:38 +0300

    use create elements

--- lesson-1/src/components/Welcome.jsx
+++ lesson-1/src/components/Welcome.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 class Welcome extends Component{
     render(){
-        return <h2>CLass Component</h2>
+        // return <h2>CLass Component</h2>
+        return React.createElement('div',{id:'hello'},React.createElement('h1',{className:'display-4'},'Hey Yeah'))
     }
 
 };
